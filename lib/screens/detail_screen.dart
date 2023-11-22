3b46741410e0d6fc212715ad276e9f058e2024c8
rickycang <150101663+rickycang@users.noreply.github.com>
2023-11-22 12:58:37 +0700

    Add files via upload

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_riverpod/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DetailScreen extends ConsumerStatefulWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  static String routeName = '/detail_screen';
+
+  @override
+  ConsumerState<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends ConsumerState<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final counterProv = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Detail'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button in the previous screen this many times:',
+            ),
+            Text(
+              counterProv.getCounter().toString(),
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                // Use the provider to interact with the state
+                counterProv.resetCounter();
+              },
+              child: const Text('Clear Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
